e5d3fd21b67fb9dfdc1e1429b609cb8e267251f2
Kenta Komai <kaede06152000@gmail.com>
2015-12-20 19:08:06 +0900

    add settings for stylus

--- gulp_tasks/build.babel.js
+++ gulp_tasks/build.babel.js
@@ -1,9 +1,16 @@
 import gulp from 'gulp';
 import webpack from 'gulp-webpack';
 import webpackConfig from '../webpack.config.js';
+import stylust from 'stylus';
 
 gulp.task('webpack', () => {
   return gulp.src('')
         .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('./assets/dist/javascript'));
+        .pipe(gulp.dest('./assets/dist/javascript/'));
+});
+
+gulp.task('stylus', () => {
+  return gulp.src('./assets/sources/stylus/*')
+      .pipe(stylus())
+      .pipe(gulp.dest('./assets/dist/css/'));
 })
--- gulp_tasks/watch.babel.js
+++ gulp_tasks/watch.babel.js
@@ -4,4 +4,5 @@ import path from 'path';
 
 gulp.task('watch', () => {
   gulp.watch(['assets/sources/**/*.js'], ['webpack']);
+  gulp.watch(['assets/sources/**/*.styl'], ['stylus']);
 });
