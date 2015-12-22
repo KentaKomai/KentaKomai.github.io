938c8f23ba3a7b3ac16a24a27fc1145cca70c36d
Kenta Komai <komaikenta@team-lab.com>
2015-12-22 12:48:52 +0900

    add stylesheet with stylus

--- gulp/task/build.babel.js
+++ gulp/task/build.babel.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
 import webpack from 'gulp-webpack'
 import webpackConfig from '../../webpack.config.js'
-import stylus from 'stylus'
+import stylus from 'gulp-stylus'
 
 gulp.task('webpack', () => {
   return gulp.src('')
