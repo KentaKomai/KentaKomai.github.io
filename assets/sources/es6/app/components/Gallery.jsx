e7b06913d431e182148471b29a39c4a3649bbe41
kkomai <komaikenta@team-lab.com>
2015-12-29 17:56:11 +0900

    merge

--- assets/sources/es6/app/components/Gallery.jsx
+++ assets/sources/es6/app/components/Gallery.jsx
@@ -4,7 +4,7 @@ export default class Gallery extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   render() {
     return (
       <div>
--- assets/sources/es6/app/components/Home.jsx
+++ assets/sources/es6/app/components/Home.jsx
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   render() {
     return (
       <div>
